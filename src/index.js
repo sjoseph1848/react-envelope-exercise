0b2d8d223d8df1e309640c5d739066809390a4f2
Samuel <sjoseph1848@gmail.com>
2019-04-24 06:29:20 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PropTypes from 'prop-types';
+
+function Envelope() {
+  return(
+    <div className="envelope">
+      Hello
+    </div>
+  )
+}
+
+ReactDOM.render(<Envelope />, document.getElementById('root'));
+
+
