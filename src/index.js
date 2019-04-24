ff12d5cac565432c2e30e6c3388000b5edb69a8e
Samuel <sjoseph1848@gmail.com>
2019-04-24 06:58:46 -0500

    added sender information

--- src/index.js
+++ src/index.js
@@ -3,14 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types';
 
-function Envelope() {
+function Envelope({envInfo}) {
   return(
     <div className="envelope">
-      Hello
+      <Sender sender={envInfo.sender}/>
     </div>
   )
 }
 
-ReactDOM.render(<Envelope />, document.getElementById('root'));
+var addressInfo = {
+    sender:{
+      senderName: "Mr. Sam",
+      senderAddress: "456 Lane",
+      senderState: "Denver, CO 80202"
+      },
+    receiver: {
+      receiverName:"Mr.Bob",
+      receiverAddress:"902 Pembrook Loop",
+      receiverState:"San Jose, CA 90210"
+    }
+  
+  
+}
+
+function Sender({sender}){
+  const {senderName, senderAddress, senderState} = sender;
+  return (
+    <div className="send">
+      <span>
+      {senderName}
+      </span> <br></br>
+      <span>
+      {senderAddress}
+      </span><br></br>
+      <span>
+      {senderState}
+      </span><br></br>
+  </div>  
+  )
+}
+
+ReactDOM.render(<Envelope envInfo={addressInfo}/>, document.getElementById('root'));
 
 
